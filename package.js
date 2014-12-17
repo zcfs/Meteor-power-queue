c4e4b84054eeffff2df3249081c2f652aae1c20f
Morten Henriksen <mh@gi2.dk>
2014-12-17 20:47:04 +0100

    bump versions

--- package.js
+++ package.js
@@ -8,11 +8,11 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
 
-  api.use(['deps', 'cfs:reactive-property@0.0.3'], ['client', 'server']);
+  api.use(['deps', 'cfs:reactive-property@0.0.4'], ['client', 'server']);
 
   // We let the user decide what spinal queue to use - We support both
   // reactive-list and micro-queue they obey the spinal-queue spec
-  api.use(['cfs:reactive-list@0.0.8', 'cfs:micro-queue@0.0.5'], ['client', 'server'], { weak: true });
+  api.use(['cfs:reactive-list@0.0.9', 'cfs:micro-queue@0.0.6'], ['client', 'server'], { weak: true });
 
   api.export && api.export('PowerQueue');
   api.addFiles(['power-queue.js'], ['client', 'server']);
