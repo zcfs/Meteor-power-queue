738d73351956974a6554d4b46fff5232cb2b9305
Morten Henriksen <mh@gi2.dk>
2014-12-17 14:17:11 +0100

    'cfs:standard-packages'

--- package.js
+++ package.js
@@ -1,26 +1,27 @@
 Package.describe({
   name: 'cfs:power-queue',
   version: '0.0.1',
-  summary: "PowerQueue is a powerful tool for handling async tasks, throtling etc."
+  summary: "PowerQueue is a powerful tool for handling async tasks, throtling etc.",
+  git: 'https://github.com/CollectionFS/Meteor-power-queue.git'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
 
-  api.use(['deps', 'cfs:reactive-property@0.0.0'], ['client', 'server']);
+  api.use(['deps', 'cfs:reactive-property@0.0.3'], ['client', 'server']);
 
   // We let the user decide what spinal queue to use - We support both
   // reactive-list and micro-queue they obey the spinal-queue spec
-  api.use(['cfs:reactive-list@0.0.0', 'cfs:micro-queue@0.0.0'], ['client', 'server'], { weak: true });
+  api.use(['cfs:reactive-list@0.0.8', 'cfs:micro-queue@0.0.4'], ['client', 'server'], { weak: true });
 
   api.export && api.export('PowerQueue');
-  api.add_files(['power-queue.js'], ['client', 'server']);
+  api.addFiles(['power-queue.js'], ['client', 'server']);
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(['cfs:power-queue', 'cfs:reactive-list']);
   api.use('test-helpers', ['server', 'client']);
   api.use('tinytest');
 
-  api.add_files('tests.js');
+  api.addFiles('tests.js');
 });
